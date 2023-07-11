888284e2b62bd44d642d0d2a4eed3f7753e10ff5
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2023-07-11 07:22:06 +0530

    String - 88 question done on leetcode

--- Striver SDE Sheet/Array/Medium/50. Pow(x, n).java
+++ Striver SDE Sheet/Array/Medium/50. Pow(x, n).java
@@ -1,4 +1,4 @@
-//overflow error
+//overflow error 2^31 to 2^31-1, when we convert most negative to poistive it will be mor than one of most posistive number in integer. so will give overflow error.
 class Solution {
     public double myPow(double x, int n) {
         double ans = 1.0;
