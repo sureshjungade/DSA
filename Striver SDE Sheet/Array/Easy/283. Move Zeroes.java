10cc327fdf432a0a13202ceb570be1a83356e322
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2023-07-20 07:48:25 +0530

    String - 95 question done on leetcode

---
+++ Striver SDE Sheet/Array/Easy/283. Move Zeroes.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
+        
+       int i = 0;
+       int j = 1;
+
+       while(i<n && j<n){
+           if(arr[i]==0){
+               if(arr[j]!=0){
+                   arr[i] = arr[j];
+                   arr[j] = 0;
+                   i++;
+                   j++;
+               }else{
+                   j++;
+               }
+           }else{
+               i++;
+               j++;
+           }
+       }
+        
+
+       //optimized version :
+
+       int n = arr.length;
+         int i = 0;
+        for(int num : arr){
+            if(num!=0) arr[i++] = num;
+        }
+        while(i<n){
+            arr[i++] = 0;
+        }
+ 
+    }
+}
